a89a8862373d60485d9830a136941424ddd185d0
Sanni Isokirmo <sanni.isokirmo@helsinki.fi>
2023-11-02 03:36:18 +0200

    tests to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,33 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_otetaan_liikaa(self):
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(5), 0)
+
+    def test_negatiivinen_lisays(self):
+        self.varasto.lisaa_varastoon(-2)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_tulostus(self):
+        self.assertEqual(str(self.varasto), 'saldo = 0, vielä tilaa 10')
+
+    def test_virheellinen_alku(self):
+        toinen_varasto = Varasto(-2,3)
+        self.assertEqual(str(toinen_varasto), 'saldo = -2, vielä tilaa 2.0')
+
+    def test_virheellinen_alku_saldo(self):
+        toinen_varasto = Varasto(2,-2)
+        self.assertEqual(str(toinen_varasto), 'saldo = 0.0, vielä tilaa 2.0')
+
+    def test_lisaa_varastoon_suurempi(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertEqual(str(self.varasto), 'saldo = 10, vielä tilaa 0')
+
+    def test_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(1)
+        self.varasto.ota_varastosta(-2)
+        self.assertEqual(str(self.varasto), 'saldo = 1, vielä tilaa 9')
+
+
